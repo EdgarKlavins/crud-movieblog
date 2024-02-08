61e151106c064bc1fdf8be4b0e3319fa4454ad3d
EdgarKlavins <klavins.e@gmail.com>
2024-02-08 23:35:07 +0000

    added cards to display movie title, linked database request

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,21 +2,21 @@ from taskmanager import db
 
 
 class User(db.Model):
-    # Schema for the User model
+    # User model
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     
     def __repr__(self):
-        # __repr__ to represent itself in the form of a string
+        
         return f"User('{self.username}', '{self.email}')"
 
 
 class Movie(db.Model):
-    # Schema for the Movie model
+    #Movie model
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)  # Adjusted attribute name
+    title = db.Column(db.String(100), nullable=False)  
     description_id = db.Column(db.Integer, db.ForeignKey('description.id'))
     genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'))
     year_id = db.Column(db.Integer, db.ForeignKey('year.id'))
@@ -26,7 +26,7 @@ class Movie(db.Model):
         return f"Movie('{self.title}')"
 
 class Genre(db.Model):
-    # Schema for the Genre model
+    # Genre model
     id = db.Column(db.Integer, primary_key=True)
     genre_name = db.Column(db.String(50), unique=True, nullable=False)
     movies = db.relationship('Movie', backref='genre', lazy=True)
@@ -38,7 +38,7 @@ class Genre(db.Model):
 
 
 class Year(db.Model):
-    # Schema for the Year model
+    #Year model
     id = db.Column(db.Integer, primary_key=True)
     year_value = db.Column(db.Integer, unique=True, nullable=False)
     movies = db.relationship('Movie', backref='year', lazy=True)
--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,13 +10,14 @@ def home():
 
 @app.route("/movies")
 def movies():
-    return render_template("movies.html")
+    movies = list(Movie.query.order_by(Movie.title).all())
+    return render_template("movies.html", movies=movies)
 
 
 @app.route("/add_movie", methods=["GET", "POST"])
 def add_movie():
     if request.method == "POST":
-        movie = Movie(title=request.form.get("movie_name"))
+        movie = Movie(title=request.form.get("title"))
         db.session.add(movie)
         db.session.commit()
         return redirect(url_for("movies"))
