26519ee86c149a6021d878f771a5517b1bad298f
EdgarKlavins <klavins.e@gmail.com>
2024-02-11 15:35:52 +0000

    altered models file, because hash generated passwords need more space

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,8 +5,7 @@ class User(db.Model):
     # User model
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     movies = db.relationship("Movie", backref="user_movies", 
                              cascade="all, delete", lazy=True)
     
